8e4a36601e5b03a63a484159ee81de12773007b6
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-09-03 01:23:49 +0300

    finish project

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,51 @@
 $(function(){
 
+	$(".tab-mobile").click(function() {
+	$(".tab-mobile").removeClass("active").eq($(this).index()).addClass("active");
+ 	$(".tab-content").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+
+$('.slider').slick({
+  slidesToShow: 1,
+  vertical: true,
+  dots: true,
+  prevArrow: $(''),
+  nextArrow: $(''),
+  responsive: [
+   {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      dots: false,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+      vertical: false
+    }
+  }
+ 
+
+  ]
+
+ 
+  
+});
+
+
+// $( window ).on('resize',function() {
+//      $('.slider').slick({
+//   slidesToShow: 1,
+//   vertical: true,
+//   dots: true,
+//   arrows: false
+
+ 
+  
+// });
+
+//   });
 
 
 });
+
+
